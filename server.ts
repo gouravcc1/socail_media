03988a2f9a3f903346734525a0e357186dbad289
Gourav Choudhary <gouravcc1@gmail.com>
2023-08-12 21:18:34 +0530

    zz

--- server.ts
+++ server.ts
@@ -2,13 +2,12 @@ import * as dotenv from "dotenv";
 // import mongodb from 'mongodb';
 import * as mongoose from "mongoose";
 dotenv.config({ path: "./config.env" });
-import app from "./app";
 const port = Number(process.env.PORT);
-
 const DB: string = (process.env.DATA_BASE ? process.env.DATA_BASE : "").replace(
   "<password>",
   process.env.DATA_BASE_PASSWORD ? process.env.DATA_BASE_PASSWORD : ""
-);
+  );
+  import app from "./app";
 
 mongoose.connect(DB, {}).then((con) => {
   console.log("success conection");
